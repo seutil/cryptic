0d4078d375e0c1777310f6a0bc72a9fd76ed7d4b
seutil <seutil@inbox.ru>
2023-08-29 14:32:04 +0300

    Add implementation for:     * __getitem__     * __setitem__     * __delitem__ BaseGroup magic methods

--- cryptic/core/groups.py
+++ cryptic/core/groups.py
@@ -1,13 +1,16 @@
+import abc
+import items
 from datetime import datetime
 
 
-class BaseGroup:
+class BaseGroup():
     ''' Base class for all groups '''
 
     def __init__(self, name: str, description=''):
         self._name = name
         self._description = description
         self._modification_time = datetime.now()
+        self._items = []
 
     @property
     def name(self) -> str:
@@ -35,3 +38,25 @@ class BaseGroup:
 
     def _update(self):
         self._modification_time = datetime.now()
+
+    @abc.abstractmethod
+    def _check_item(self, item: items.BaseItem):
+        raise NotImplementedError(f'Class "{self.__class__.name}" is not implemented')
+
+    def __getitem__(self, key: int) -> items.BaseItem:
+        return self._items[key]
+
+    def __setitem__(self, key: int, value: items.BaseItem):
+        self._check_item(value)
+        # TODO: check that item's group
+        # TODO: chat that item is not in the list
+        self._items[key] = value
+        self._update()
+
+    def __delitem__(self, key: int):
+        # TODO: set item group to None
+        del self._items[key]
+        self._update()
+
+    def __len__(self) -> int:
+        return len(self._items)
