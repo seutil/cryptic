6d2ecd5325f56644555244615a2bad16554fb453
seutil <seutil@inbox.ru>
2023-08-29 10:52:49 +0300

    Add parameters

--- tests/items/test_login_item.py
+++ tests/items/test_login_item.py
@@ -3,15 +3,19 @@ from datetime import datetime
 from cryptic.core.items import LoginItem
 
 
-def test_constructor_valid():
+@pytest.mark.parametrize('name, login, password, email', [
+    ('', 'insideewquick-witted', 'DF3n4nYXk8jK', 'benjaminmiller@email.com'),
+    ('name', 'psstwhereasfan', 'D42y9N9VMtpg', 'rachelmoore77@email.com'),
+])
+def test_constructor_valid(name, login, password, email):
     start = datetime.now()
     try:
-        item = LoginItem('Login #1', 'login1', 'my-password')
+        item = LoginItem(name, login, password, email)
     except Exception as e:
         pytest.fail(f'Exception raised: {e}')
 
-    assert item.name == 'Login #1'
-    assert item.login == 'login1'
-    assert item.password == 'my-password'
-    assert item.email == ''
+    assert item.name == name
+    assert item.login == login
+    assert item.password == password
+    assert item.email == email
     assert start <= item.modification_time <= datetime.now()
